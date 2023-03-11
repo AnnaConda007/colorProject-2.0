20d1aecd49d1a69e610fa2bff0785b3952b6faa1
Anna Hrulckova <annahrulkova@yandex.ru>
2023-03-11 15:04:53 +0400

    добавлен блок с аккордионом

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import filter from './modules/filter.js'
-import modalBlock from './modules/moduls.js'
-
+//import modalBlock from './modules/moduls.js'
+import accordion from './modules/accordion.js'
 window.addEventListener('DOMContentLoaded', () => {
   filter()
-  modalBlock('.triger-pink', '.form-substrate-pink', '.closeBtn')
-  modalBlock('.triger-red', '.form-substrate-red', '.closeNextBtn')
-  modalBlock('.menu-btn', '.menu', '.closeMenu')
+ // modalBlock('.triger-pink', '.form-substrate-pink', '.closeBtn')
+  //modalBlock('.triger-red', '.form-substrate-red', '.closeNextBtn')
+  //modalBlock('.menu-btn', '.menu', '.closeMenu')
+  accordion(".btn-accordion",".text-accordion")
 })
